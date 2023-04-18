4579d52f799d3625d6d779f8f58ad8884765141e
maahh <maahhi.k@gmail.com>
2023-04-18 19:59:36 +0330

    utils

--- data/utils/checkduration.py
+++ data/utils/checkduration.py
@@ -6,7 +6,8 @@ def print_wav_durations(directory):
     for file in glob.glob(os.path.join(directory, '*.wav')):
         audio = AudioSegment.from_wav(file)
         duration = len(audio) / 1000  # Convert to seconds
-        print(f"File: {file}, Duration: {duration:.2f} seconds")
+        sample_rate = audio.frame_rate
+        print(f"File: {file}, Duration: {duration:.2f} seconds",sample_rate)
 
 input_directory = '../raw'
 print_wav_durations(input_directory)
